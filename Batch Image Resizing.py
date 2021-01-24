8e6673b328ad716834eb8704d3873a43e0efcbce
ndhanjee <52287613+ndhanjee@users.noreply.github.com>
2021-01-23 21:12:56 -0500

    Update Batch Image Resizing.py

--- Batch Image Resizing.py
+++ Batch Image Resizing.py
@@ -1,4 +1,5 @@
-#This python file resizes all existing .jpg files in the same directory: 
+#This python file resizes all existing .jpg files in the same directory.
+#Save this Python file in the same folder as the photos and run it.
 
 import glob
 import cv2
@@ -7,9 +8,12 @@ import cv2
 images = glob.glob("*.jpg")
 
 for image in images:
-    img = cv2.imread(image, 0) #this line also converts image to 0 (grey scale), 1 (color) or -1 (color with transparency) 
+    img = cv2.imread(image, 1) #0 (grey scale), 1 (color) or -1 (color with transparency)
 
-    resized_img = cv2.resize(img, (100, 100)) #command that resizes the image to 100 x 100 pixels
+    resized_img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+
+    #command that resizes the image to a particular size (might skew image though!)
+    #resized_img = cv2.resize(img, (1000,1000))
 
     cv2.imwrite("resized_"+image, resized_img) #command that writes the image in a new file
 
